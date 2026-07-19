fbcf3449adc0650b4b546e5f84801ad0edeed436
Yaswitha <yaswithayashu2001@gmail.com>
2026-07-19 16:05:42 +0530

    corected urls files

--- django-web-app/devops/devops/urls.py
+++ django-web-app/devops/devops/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for devops project.
+"""devops URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('demo/','demo_site.html'),
+    path('demo/', include('demo.urls')),
+    path('admin/', admin.site.urls),
 ]
