3209693ccbd68ddd41a516401ceaa251903bd7e4
Yaswitha <yaswithayashu2001@gmail.com>
2026-07-19 13:07:43 +0530

    New docker django app

--- django-web-app/devops/devops/wsgi.py
+++ django-web-app/devops/devops/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dev project.
+WSGI config for Devops project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Devops.settings')
 
 application = get_wsgi_application()
